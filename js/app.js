581e24e8cd6b33ee82fdbc52b55c0d9a002b7c96
诚明小巫 <1312528339@qq.com>
2018-03-23 14:56:36 +0800

    网页

---
+++ js/app.js
@@ -0,0 +1,106 @@
+(function() {
+
+	var tableListDatas = {
+		'officialDataList': [{
+				no: "0001",
+				name: "张1",
+				sex: 0, //1男0女
+				state: 0, //1启用0关闭
+			},
+			{
+				no: "0002",
+				name: "张2",
+				sex: 1, //1男0女
+				state: 1, //1启用0关闭
+			},
+			{
+				no: "0003",
+				name: "张3",
+				sex: 1, //1男0女
+				state: 1, //1启用0关闭
+			},
+			{
+				no: "0004",
+				name: "张4",
+				sex: 0, //1男0女
+				state: 1, //1启用0关闭
+			},
+			{
+				no: "0005",
+				name: "张5",
+				sex: 0, //1男0女
+				state: 1, //1启用0关闭
+			}
+		],
+		'teamList': [{
+				name: "3D打印技术制备结构吸波材料及其性能材料及其性能规",
+				fenlei: "光电光学仪器",
+				create: "赵灵儿", //1男0女
+				uptime: "2018-01-23 14:25", //1启用0关闭
+			},
+			{
+				name: "7A52铝合金高温拉伸性能研究",
+				fenlei: "光子学技术",
+				create: "张三", //1男0女
+				uptime: "2017-02-23 14:25", //1启用0关闭
+			},
+			{
+				name: "2B06铝合金阳极氧化工艺和性能研究",
+				fenlei: "信息光学",
+				create: "赵四", //1男0女
+				uptime: "2017-12-23 14:25", //1启用0关闭
+			},
+			{
+				name: "2B06铝合金阳极氧化工艺和性能研究",
+				fenlei: "信息光学",
+				create: "赵四", //1男0女
+				uptime: "2017-12-23 14:25", //1启用0关闭
+			},
+			{
+				name: "2B06铝合金阳极氧化工艺和性能研究",
+				fenlei: "信息光学",
+				create: "赵四", //1男0女
+				uptime: "2017-12-23 14:25", //1启用0关闭
+			},
+			{
+				name: "2B06铝合金阳极氧化工艺和性能研究",
+				fenlei: "信息光学",
+				create: "赵四", //1男0女
+				uptime: "2017-12-23 14:25", //1启用0关闭
+			}
+			,
+			{
+				name: "2B06铝合金阳极氧化工艺和性能研究",
+				fenlei: "信息光学",
+				create: "赵四", //1男0女
+				uptime: "2017-12-23 14:25", //1启用0关闭
+			},
+			{
+				name: "2B06铝合金阳极氧化工艺和性能研究",
+				fenlei: "信息光学",
+				create: "赵四", //1男0女
+				uptime: "2017-12-23 14:25", //1启用0关闭
+			}
+		],
+
+	}
+
+	var commonData = {};
+	var commonFuns = {};
+	commonData.tableListDatas = tableListDatas.officialDataList;
+	commonData.teamList = tableListDatas.teamList;
+
+	commonData.colors = ['#00D2D2', '#ff6600', '#FF0080', '#1DCAE2', '#FA96F8', '#26D92F'];
+	commonFuns.mobilecheck = function() {
+		var check = false;
+		(function(a) {
+			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+		})(navigator.userAgent || navigator.vendor || window.opera);
+		return check;
+	};
+	commonFuns.ie8check = function() {
+		return navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0";
+	};
+	window.commonData = commonData;
+	window.commonFuns = commonFuns;
+})();
